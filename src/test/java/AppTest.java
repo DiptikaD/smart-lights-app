be2142398ea76ac383420458ebfadb7af781eb6d
Devi <diptikadevi@gmail.com>
2024-09-15 14:38:46 -0400

    initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp(){
+
+        int actual = 0;
+
+        Assert.assertEquals(actual, 0);
+    }
+
+}
