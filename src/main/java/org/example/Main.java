933c5ca0e8c9690d3d60a40e75b2091eed4ca3d5
Devi <diptikadevi@gmail.com>
2024-09-16 21:42:35 -0400

    updated main for jframe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,29 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame jframe = new JFrame("pretty lights");
+            jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            jframe.setSize(100,40);
 
-        System.out.printf("Hello and welcome!");
+            List<LightsModel> lights = new ArrayList<>();
+            lights.add(new LightsModel(Color.green, 100, true));
+            lights.add(new LightsModel(Color.BLUE, 60, true));
+            lights.add(new LightsModel(Color.magenta, 100, false));
+            lights.add(new LightsModel());
+            lights.add(new LightsModel(Color.magenta, 15, true));
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+            LightsViewer lightsViewer = new LightsViewer(lights);
+            jframe.add(lightsViewer, BorderLayout.CENTER);
+
+            jframe.pack();
+            jframe.setVisible(true);
+        }) ;
     }
 }
